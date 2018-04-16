7b92b9a2b8f51212ee29028e30971eedca41326e
Chaitanya Maniar <chaitanya.maniar1123@gmail.com>
2018-04-15 23:35:07 -0400

    Updated Factor graph construction and simulated shilling attack

--- final_Chaitanya.py
+++ final_Chaitanya.py
@@ -4,6 +4,7 @@ import sys
 import numpy as np
 from pgmpy.models import FactorGraph
 from pgmpy.factors.discrete import DiscreteFactor
+from pgmpy.factors.continuous import ContinuousFactor
 from data import build_movies_dict, generate_matrix
 
 # Set paths
@@ -17,7 +18,8 @@ beta_1 = -1
 tau_1 = 0.5
 beta_2 = 1
 tau_2 = 1.5
-
+min_rating = 1
+max_rating = 5
 
 # User-item rating matrix
 movies_dict = build_movies_dict(movies_data)
@@ -28,8 +30,10 @@ R = generate_matrix(ratings_data, movies_dict)
 num_users = np.shape(R)[0]
 num_items = np.shape(R)[1]
 
+#def build_model(num_users, num_items):
 
-
+# Initialize Factor Graph
+G = FactorGraph()
 
 # Create nodes : node_list = ['m1', 'm2', 'm3', 't1', 't2', 't3']
 user_nodes = []
@@ -42,91 +46,85 @@ for i in range(1, num_items+1):
 
 
 # Spam Users and Target Items Initializations
-m = np.random.rand(num_users)
-m = [1 if i > 0.5 else 0 for i in m]
+# m = np.random.rand(num_users)
+m = [1 if i > 0.5 else 0 for i in np.random.rand(num_users)]
+# user_nodes = m
 
-t = np.random.rand(num_items)
-t = [1 if i > 0.5 else 0 for i in t]
-
-
-# Initialize Factor Graph
-G = FactorGraph()
+# t = np.random.rand(num_items)
+t = [1 if i > 0.5 else 0 for i in np.random.rand(num_items)]
+# item_nodes = t
 
 # Add Nodes to Factor Graph
 G.add_nodes_from(user_nodes)
 G.add_nodes_from(item_nodes)
 
 
-# Item Rating Bias
-min_rating = 1
-max_rating = 5
-
-rating_bias = []
-
-for i in range(num_items):
-    r_ui = [R[u, i] for u in range(num_users) if R[u,i] != 0]
-    m_i = [m[u] for u in range(num_users) if R[u,i] == 5]
-
-    first = sum(r_ui)/(len(r_ui) + 1e-9)
-    second = (sum(r_ui) - max_rating * sum(m_i)) / (len(r_ui) - sum(m_i) + 1e-9)
-
-    rating_bias.append(np.abs(first - second))
-
-# Factor (f) over each Item
-def almost_softmax(x, scale, feature, threshold):
+# Factor Helper Functions
+def almost_sigmoid(x, scale, feature, threshold):
     return 1/(1 + np.exp(np.power(-1,(1-x)) * scale * (feature - threshold)))
 
+def item_rating_bias(R, num_users, num_items):
+
+    rating_bias = []
+
+    for i in range(num_items):
+        r_ui = [R[u, i] for u in range(num_users) if R[u,i] != 0]
+        m_i = [m[u] for u in range(num_users) if R[u,i] == 5]
+
+        first = sum(r_ui)/(len(r_ui) + 1e-9) #Avoid by 0 division
+        second = (sum(r_ui) - max_rating * sum(m_i)) / (len(r_ui) - sum(m_i) + 1e-9)
+
+        rating_bias.append(np.abs(first - second))
+
+    return rating_bias
+
+def mean_var(R, num_users, num_items):
+
+    mean_var = []
+    r_i_bar = np.average(R, axis=0)
+
+    for u in range(num_users):
+        I_u_subset = [R[u, i] for i in range(num_items) if R[u,i] !=0 and R[u,i] != 5]
+        avg_u_subset = [r_i_bar[i] for i in range(num_items) if R[u,i] !=0 and R[u,i] != 5]
+        mean_var_value = [(first - second)**2 for first, second in zip(I_u_subset, avg_u_subset)]
+        mean_var.append(sum(mean_var_value)/len(mean_var_value))
+
+    return mean_var
+
+
+# Init factors and factor_vals
 f = []
-h = []
-psi = np.var(R,axis = 0)
-
-for item in range(num_items):
-    f.append(almost_softmax(t[item], alpha_t, rating_bias[item], delta_r))
-    h.append(almost_softmax(t[item], beta_2, psi[item], tau_2))
-
-sys.exit(0)
-# Phi for Every User
-phi = []
-average_item = np.average(R,axis=0)
-
-for user in range(num_users):
-    ratings_u = R[user,:]
-    I_non_max_u = ratings_u[ratings_u<max_rating]
-    phi_u = sum(np.power(ratings_u[I_non_max_u] - average_item[I_non_max_u],2))/(sum(I_non_max_u))
-    phi.append(phi_u)
-    
-
-# Factor (g) over Each User
-beta_1 = 0.1
-tau_1 = 0.1
-
-g = []
-
-for user in range(num_users):
-    g_u = 1/(91 + np.exp(np.power(-1,1-m[user])*beta_1*(phi[user] - tau_1)))
-    g.append(g_u)
-
-
-
-#Create Factors
 g = []
 h = []
-f = []
 
+# Features
+rating_bias = item_rating_bias(R, num_users, num_items)
+psi = np.var(R, axis = 0)
+phi_u = mean_var(R, num_users, num_items)
 
+# Create factors
+# for item in range(num_items):
+#     f_values.append(almost_sigmoid(t[item], alpha_t, rating_bias[item], delta_r))
+#     h_values.append(almost_sigmoid(t[item], beta_2, psi[item], tau_2))
+#
+# for user in range(num_users):
+#     g_values.append(almost_sigmoid(m[user], beta_1, phi_u[user], tau_1))
 
+# Create Factors
+for user, user_node in enumerate(user_nodes):
+    # g.append(DiscreteFactor([user_node], [2], np.ones(2)))
+    g.append(ContinuousFactor([user_node], almost_sigmoid(user_node, beta_1, phi_u[user], tau_1)))
 
-for node in user_nodes:
-    g.append(DiscreteFactor([node], [2], np.ones(2)))
+for item, item_node in enumerate(item_nodes):
+    # h.append(DiscreteFactor([item_node], [2], np.ones(2)))
+    h.append(ContinuousFactor([item_node], almost_sigmoid(item_node, beta_2, psi[item], tau_2)))
 
-for node in item_nodes:
-    h.append(DiscreteFactor([node], [2], np.ones(2)))
+for user_node in user_nodes:
+    for item_node in item_nodes:
+        # f.append(DiscreteFactor([user,item], [2,2], np.ones([2,2])))
+        f.append(ContinuousFactor([user_node, item_node], almost_sigmoid(item_node, alpha_t, rating_bias[item], delta_r)))
 
-for user in user_nodes:
-    for item in item_nodes:
-        f.append(DiscreteFactor([user,item], [2,2], np.ones([2,2])))
-
-#Add factors to graph
+# Add factors to graph
 for factor in g:
     G.add_factors(factor)
 for factor in h:
@@ -134,7 +132,7 @@ for factor in h:
 for factor in f:
     G.add_factors(factor)
 
-#Add edges
+# Add edges
 for idx in range(len(user_nodes)):
     G.add_edge(user_nodes[idx], g[idx])
 
@@ -145,5 +143,3 @@ for idx in range(len(item_nodes)):
 for i in range(len(f)):
     for j in range(len(user_nodes)):
         G.add_edge(f[i], user_nodes[j])
-
-#todo: 1) Change discrete factors to continuous factors! 2) Code rating bias, f, g, h
